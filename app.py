4461a752d765495ab327d1ae0051caa1c776377d
Divyansh Soni <divyanshsoni797@gmail.com>
2025-07-21 15:39:11 +0530

    🚀 Add files for Render deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 #importing librareries-->
 import pickle
 import numpy as np
+import os
 
 # Initialize app
 app = Flask(__name__)
@@ -46,4 +47,6 @@ def predict():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
